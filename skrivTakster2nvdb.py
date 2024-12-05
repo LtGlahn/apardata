62e4e5cdd82870a212694139314026f99c69dd7e
LtGlahn <jensen72@gmail.com>
2024-12-05 08:20:23 +0100

    Bedre automatikk

--- skrivTakster2nvdb.py
+++ skrivTakster2nvdb.py
@@ -9,7 +9,17 @@ import requests
 
 if __name__ == '__main__': 
     data = requests.get( 'https://langbein.npra.io/apardata/bomstasjon_endringssett.json' ).json()
-    endr = skrivnvdb.endringssett( data )
-    endr.forbindelse.login( miljo='prodskriv' )
-    endr.registrer()
-    endr.startskriving()
+    assert 'delvisOppdater' in data, "Dette er ikke endringssett for delvisOppdater"
+    assert 'vegobjekter' in  data['delvisOppdater'], "Dette er ikke endringssett for delvisOppdater - mangler vegobjekter"
+    assert isinstance(  data['delvisOppdater']['vegobjekter'], list  ), "vegobjekter-elementet må være en liste"
+    if len( data['delvisOppdater']['vegobjekter'] ) > 0: 
+
+        print( f"{len( data['delvisOppdater']['vegobjekter']) } bomstasjoner har fått nye takster, lagrer til NVDB")
+
+        endr = skrivnvdb.endringssett( data )
+        endr.forbindelse.login( miljo='prodskriv' )
+        endr.registrer()
+        endr.startskriving()
+
+    else: 
+        print( f"Ingen takstoppdatering i dag")
